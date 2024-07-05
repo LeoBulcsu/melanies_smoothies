44265262f880f2189010c84c2ba7972e8b970881
LeoBulcsu <136447924+LeoBulcsu@users.noreply.github.com>
2024-07-05 19:20:59 +0200

    Update streamlit_app.py
    addinf the search on column

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
+
 
 # Write directly to the app
 st.title(":cup_with_straw:  Customize Your Smoothie  :cup_with_straw:")
@@ -20,9 +22,14 @@ if name_on_order:
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+#CONVERT THE SNOWPARK DF TO A PANDAS DF SO WE CAN USE THE LOC FUNCTION
+pd_df = my_dataframe.to_pandas()         
+st.dataframe(pd_df)
+st.stop
+                                                                                             
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients;',
     my_dataframe,
